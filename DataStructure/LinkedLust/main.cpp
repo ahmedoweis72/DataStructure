f0b2fa8e4b95d64eaecda6247e06d0df456bcd00
Ahmed <144798780+ahmedoweis72@users.noreply.github.com>
2024-12-17 09:01:15 +0200

    Update main.cpp

--- DataStructure/LinkedLust/main.cpp
+++ DataStructure/LinkedLust/main.cpp
@@ -7,15 +7,14 @@ int main()
     LinkedList l;
     l.add(5);
     l.add(6);
-    
     l.add(7);
     l.add(8);
-    // l.insertAfter(7,10);
-    // l.insertBefore(7,14);
-    // l.printLinkedList();
+    l.insertAfter(7,10);
+    l.insertBefore(7,14);
+    l.printLinkedList();
     
-    cout<<l.getDataByIndex(0)<<endl;
-    // cout<<l.getCount()<<endl;
+    cout<<l.getDataByIndex(1)<<endl;
+    cout<<l.getCount()<<endl;
 
     return 0;
 }
